b5aafb1a6e37ad620254ed5f569025b9c0a6fe9a
mutahhirkhan <mutahhir199@gmail.com>
2021-04-15 12:23:29 +0500

    routes integrated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { Fragment } from "react";
+import "./Navbar.css";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const MenuITem = ({ children, to = "#", ...restProps }) => (
+  <Fragment>
+    <Link to={to}>
+      <Typography
+        style={{
+          display: "flex",
+          justifySelf: "flex-start",
+          width: "max-content",
+        }}
+        variant="h5"
+      >
+        {children}
+      </Typography>
+    </Link>
+  </Fragment>
+);
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="item1">Logo</div>
-            <div className="item2">Auth</div>
-            <div className="item3">Map</div>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <MenuITem to="/">App</MenuITem>
+      <MenuITem to="/authentication">Login</MenuITem>
+      <MenuITem to="/">Map</MenuITem>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
