d5fb1052ad84bf0a132c5f8aceeb1761a03b3c0b
mutahhirkhan <mutahhir199@gmail.com>
2021-04-15 13:12:44 +0500

    nothing

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import wallpaper from "src/Assets/wallpaper.jpg";
+
+const useStyle = makeStyles((theme) => ({
+  root: {},
+  wallpaper: {
+    height: `calc(100vh - var(--navbar-height))`,
+  },
+}));
 
 const Auth = () => {
-    return (
-        <div>
-            <h1>Auth</h1>
-        </div>
-    )
-}
+  const classes = useStyle();
+  return (
+    <div
+      className={classes.wallpaper}
+      style={{
+        background: `url(${wallpaper}) no-repeat center / cover`,
+      }}
+    >
+      <h1>Auth</h1>
+    </div>
+  );
+};
 
-export default Auth
+export default Auth;
